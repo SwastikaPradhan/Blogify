a7e8eb4a10aaa3ecc5db0fe9f2d6f0daf0cef13d
SwastikaPradhan <swastikapradhan51@gmail.com>
2024-11-15 13:27:44 +0530

    first commit

---
+++ common/dist/index.d.ts
@@ -0,0 +1,51 @@
+import z from "zod";
+export declare const signupInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export type SignupInput = z.infer<typeof signupInput>;
+export declare const signinInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export type SigninInput = z.infer<typeof signupInput>;
+export declare const CreateBlogPost: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export type CreateBlogPost = z.infer<typeof CreateBlogPost>;
+export declare const updateBlogPost: z.ZodObject<{
+    id: z.ZodNumber;
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    id: number;
+}, {
+    title: string;
+    content: string;
+    id: number;
+}>;
+export type updateBlogPost = z.infer<typeof updateBlogPost>;
